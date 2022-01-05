ed328f3d08790ee5aacc25e8e34bb50bb84c9f0f
regissanme <rsanme@gmail.com>
2022-01-04 23:11:21 -0300

    update PersonController post method

--- src/main/java/br/com/dio/personapi/controller/PersonController.java
+++ src/main/java/br/com/dio/personapi/controller/PersonController.java
@@ -1,8 +1,10 @@
 package br.com.dio.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.dio.personapi.dto.response.MessageResponseDTO;
+import br.com.dio.personapi.entity.Person;
+import br.com.dio.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Projeto: dio-desafio-projeto-person-api
@@ -17,8 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBook(){
-        return "API Test";
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID "+savedPerson.getId())
+                .build();
     }
 }
